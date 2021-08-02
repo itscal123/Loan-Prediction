ea794d4c599ecaf8119eb0bd38af390aa69fb95d
itscal123 <calvin.kory@gmail.com>
2021-08-02 11:17:17 -0700

    Update web_api.py

--- web_api.py
+++ web_api.py
@@ -4,20 +4,35 @@ import knn
 import random_forest
 import naive_bayes
 import neural_network
-from get_data import variables, df
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.decomposition import PCA
 import text
+import numpy as np
 
-train_set, test_set = train_test_split(df, test_size=0.1)
-y_train, y_test = train_set["MIS_Status"], test_set["MIS_Status"]
-X_train, X_test = train_set.loc[:, df.columns != "MIS_Status"], test_set.loc[:, df.columns != "MIS_Status"]
-norm = MinMaxScaler().fit(X_train)
-X_train_norm = norm.transform(X_train)
-X_test_norm = norm.transform(X_test)
-pca = PCA(n_components=0.95)
-X_test_reduced = pca.fit_transform(X_test)
+# Dataframe of the categories and their descriptions
+variables = pd.DataFrame({
+    "Variable Name": ["LoanNr_ChkDgt", "Name", "City", "State", "Zip", "Bank", "BankState",
+                        "NAICS", "ApprovalDate", "ApprovalFY", "Term", "NoEmp", "NewExist",
+                        "CreateJob", "RetainedJob", "FranchiseCode", "UrbanRural", "RevLineCr",
+                        "LowDoc", "ChgOffData", "DisbursementDate", "DisbursementGross",
+                        "BalanceGross", "MIS_Status", "ChgOffPrinGr", "GrAppv", "SBA_Appv"],
+    "Description": ["Identifier Primary key", "Borrower name", "Borrower city", "Borrower state",
+                        "Borrower zip code", "Bank name", "Bank state", "North American industry classification system code",
+                        "Date SBA commitment issued", "Fiscal year of commitment", "Loan term in months", "Number of business employees",
+                        "1 = Existing business, 2 = New business", "Number of jobs created", "Number of jobs retained",
+                        "Franchise code, (00000 or 00001) = No franchise", "1 = Urban, 2 = rural, 0 = undefined",
+                        "Revolving line of credit: Y = Yes, N = No", "LowDoc Loan Program: Y = Yes, N = No", 
+                        "The date when a loan is declared to be in default", "Disbursement data", "Amount disbursed",
+                        "Gross amount outstanding", "Loan status charged off = CHGOFF, Paid in full = PIF",
+                        "Charged-off amount", "Gross amount approved by bank","SBA's guaranteed amount of approved loan"]
+})
+
+with open("test.npy", "wb") as f:
+    X_test = np.load(f)
+
+with open("reduced_test.npy", "wb") as f:
+    X_test_norm = np.load(f)
+
+with open("reduced_test.npy", "wb") as f:
+    X_test_reduced = np.load(f)
 
 
 text.title()
